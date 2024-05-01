ab7e44cc34dc8cccdb1f330b5958bfdb9a1b12e9
AR1D <84451649+Alex06052003@users.noreply.github.com>
2024-05-01 21:37:56 +0800

    Add files via upload

--- lab3.py
+++ lab3.py
@@ -1,31 +1,29 @@
-# Лабораторная работа 3
-# Манякало Александр
-# Тема: крестики-нолики 3D
-# Об игре: https://en.wikipedia.org/wiki/3D_tic-tac-toe
-
 import itertools
 import random
+import copy
 
 class TicTacToe3D:
     def __init__(self):
-        self.board = [[[None for _ in range(4)] for _ in range(4)] for _ in range(4)]
+        self.board = [[[' ' for _ in range(4)] for _ in range(4)] for _ in range(4)]
         self.players = ['X', 'O']
         self.player_names = {'X': 'Bot1', 'O': 'Bot2'}
         self.scores = {player: 0 for player in self.players}
-        self.rounds_to_play = 3
+        self.rounds_to_play = 1001
         self.rounds_played = 0
+        self.variations = {player: {} for player in self.players}
 
     def print_board(self):
-        for i in range(4):
-            print("Layer", i+1)
-            for row in self.board[i]:
-                print(" | ".join(cell if cell else ' ' for cell in row))
-                print("-" * 7)
+        for z in range(4):
+            print("Layer", z+1)
+            for row in self.board[z]:
+                print(" | ".join(cell for cell in row))
+                print("-" * 13)
             print()
 
     def make_move(self, player, x, y, z):
-        if self.board[z][y][x] is None:
+        if self.board[z][y][x] == ' ':
             self.board[z][y][x] = player
+            self.update_variations(player)
             if self.check_win(x, y, z, player):
                 self.print_board()
                 print(f"{self.player_names[player]} wins!")
@@ -55,38 +53,74 @@ class TicTacToe3D:
         return False
 
     def check_draw(self):
-        return all(self.board[z][y][x] for x, y, z in itertools.product(range(4), range(4), range(4)))
+        return all(self.board[z][y][x] != ' ' for x, y, z in itertools.product(range(4), range(4), range(4)))
 
     def random_move(self):
-        empty_cells = [(x, y, z) for z in range(4) for y in range(4) for x in range(4) if self.board[z][y][x] is None]
+        empty_cells = [(x, y, z) for z in range(4) for y in range(4) for x in range(4) if self.board[z][y][x] == ' ']
         if empty_cells:
             return random.choice(empty_cells)
         else:
             return None
 
+    def update_variations(self, player):
+        for z in range(4):
+            for y in range(4):
+                for x in range(4):
+                    if self.board[z][y][x] == ' ':
+                        variation = copy.deepcopy(self.board)
+                        variation[z][y][x] = player
+                        self.generate_variations(variation, player)
+
+    def generate_variations(self, variation, player):
+        string = self.from_list_to_string(variation)
+        if string not in self.variations[player]:
+            self.variations[player][string] = {}
+        empty_cells = [(x, y, z) for z in range(4) for y in range(4) for x in range(4) if variation[z][y][x] == ' ']
+        for x, y, z in empty_cells:
+            new_variation = copy.deepcopy(variation)
+            new_variation[z][y][x] = player
+            new_string = self.from_list_to_string(new_variation)
+            self.variations[player][string][new_string] = 1
+
+    def from_list_to_string(self, variation):
+        string = ""
+        for z in range(4):
+            for y in range(4):
+                for x in range(4):
+                    string += variation[z][y][x]
+        return string
+
+    def machine_learning(self, player):
+        possibilities = list(self.variations[player].keys())
+        for _ in range(1500):
+            for possibility in possibilities:
+                next_moves = list(self.variations[player][possibility].keys())
+                chosen_move = random.choice(next_moves)
+                self.variations[player][possibility][chosen_move] += 1
+
     def play_round(self, round_num):
         move_count = 0
-        while move_count < 4*4*4:
+        while move_count < 64:
             for player in self.players:
                 x, y, z = self.random_move()
                 if self.make_move(player, x, y, z):
                     return
-                if player == 'O':
-                    print(f"Move {move_count // 2 + 1}")
-                    self.print_board()
                 move_count += 1
 
     def play(self):
-        while self.rounds_played < self.rounds_to_play:
-            self.board = [[[None for _ in range(4)] for _ in range(4)] for _ in range(4)]
+        for _ in range(self.rounds_to_play):
+            self.board = [[[' ' for _ in range(4)] for _ in range(4)] for _ in range(4)]
             print(f"Round {self.rounds_played + 1}")
             self.play_round(self.rounds_played)
             scores_string = ", ".join([f"{self.player_names[player]}: {score}" for player, score in self.scores.items()])
             print(f"Scores: {scores_string}")
             self.rounds_played += 1
-            if self.scores['X'] == 2 or self.scores['O'] == 2:
+            if self.scores['X'] == 1000 or self.scores['O'] == 1000:
                 print("Game Over!")
                 break
+        self.machine_learning('X')
+        self.machine_learning('O')
+        print("Machine learning completed.")
 
 def main():
     game = TicTacToe3D()
